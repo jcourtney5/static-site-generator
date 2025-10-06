a1db09f87b3585f260cbc58ae13d97fb41cb3bfc
jcourtney5 <jesse.courtney5@gmail.com>
2025-10-05 23:55:32 -0600

    added function to recursively copy all files from static to public dir

--- src/main.py
+++ src/main.py
@@ -1,12 +1,44 @@
-from textnode import TextNode, TextType
+import os
+import shutil
 
 def main():
-  # test creating a TextNode
-  textNode1 = TextNode("sample text", TextType.ITALIC, None)
-  print(f"textNode1: {textNode1}")
+  build_public_folder()
 
-  textNode2 = TextNode("link to an image", TextType.IMAGE, "http://imageurl.jpg")
-  print(f"textNode2: {textNode2}")
+
+def build_public_folder():
+  root_dir = os.getcwd()
+
+  # get full paths
+  public_folder = os.path.join(root_dir, 'public')
+  static_folder = os.path.join(root_dir, 'static')
+
+  # delete public folder and recreate it
+  if os.path.exists(public_folder):
+    shutil.rmtree(public_folder)
+  os.mkdir(public_folder)
+  print("cleared public folder")
+
+  # copy entire contents from static to public
+  copy_files(static_folder, public_folder)
+
+
+def copy_files(source_dir, dest_dir):
+  items = os.listdir(source_dir)
+  for item in items:
+    # get full paths
+    item_source_full_path = os.path.join(source_dir, item)
+    item_dest_full_path = os.path.join(dest_dir, item)
+
+    if os.path.isfile(item_source_full_path):
+      # if it is a file, just copy it
+      print(f"copying {item_source_full_path} to {item_dest_full_path}")
+      shutil.copy(item_source_full_path, item_dest_full_path)
+    else:
+      # if it is a folder, then create dest folder and recursively call copy_files
+      print(f"making dir {item_dest_full_path}")
+      os.mkdir(item_dest_full_path)
+      copy_files(item_source_full_path, item_dest_full_path)
+
 
 if __name__ == "__main__":
     main()
